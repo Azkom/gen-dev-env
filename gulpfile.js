62a1c64fc671816c670844df7a801f5005e371f3
Azkom <kozma.p.bela@t-online.hu>
2018-11-04 19:49:50 +0100

    Update gulpjs file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
-require("./gulp/tasks/watch");
+/* require("./gulp/tasks/watch");
 require("./gulp/tasks/styles");
 require("./gulp/tasks/scripts");
 require("./gulp/tasks/modernizr");
-require("./gulp/tasks/build");
+require("./gulp/tasks/build"); */
+const { src, dest } = require("gulp");
+const babel = require("gulp-babel");
+exports.default = function() {
+  return src("src/*js")
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(dest("output/"));
+};
